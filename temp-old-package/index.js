12ed61d821b553ceb0518853d880ea2060b18f5e
mgomez-ext <martin.gomez.ext@redsalud.cl>
2026-01-14 07:56:41 -0300

    ..

---
+++ temp-old-package/index.js
@@ -0,0 +1,19 @@
+/**
+ * @mrs-uisystem/ui-v6 has been migrated to @atipicus/mrs-ui
+ * 
+ * Please update your imports:
+ * 
+ * OLD: import { Button } from '@mrs-uisystem/ui-v6';
+ * NEW: import { Button } from '@atipicus/mrs-ui';
+ * 
+ * See: https://www.npmjs.com/package/@atipicus/mrs-ui
+ */
+
+console.warn(
+  '@mrs-uisystem/ui-v6 has been migrated to @atipicus/mrs-ui. ' +
+  'Please update your dependencies. ' +
+  'See: https://www.npmjs.com/package/@atipicus/mrs-ui'
+);
+
+module.exports = {};
+
