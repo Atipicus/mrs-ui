fec6b512decb80876991d2cb5a372dabe56114e4
Eduardo Diaz <eduardo.diaz.ext@redsalud.cl>
2026-03-20 13:19:35 -0300

    feat(molecules): add PillTabsControl index export and types

---
+++ src/components/molecules/PillTabsControl/index.ts
@@ -0,0 +1,8 @@
+/**
+ * PillTabsControl exports
+ * @package @mrs/ui
+ */
+
+export { PillTabsControl } from './PillTabsControl';
+export type { PillTabsControlProps, PillTabItem } from './PillTabsControl.types';
+export { default } from './PillTabsControl';
